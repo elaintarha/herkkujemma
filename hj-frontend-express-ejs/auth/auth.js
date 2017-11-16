48da7056b3208b347cdebb3df6199ecc32b5564f
Jaakko Saari <elaintarha@gmail.com>
2017-11-16 15:22:03 +0200

    Add minimal auth0 login

--- hj-frontend-express-ejs/auth/auth.js
+++ hj-frontend-express-ejs/auth/auth.js
@@ -1,3 +1,4 @@
+const Auth0Strategy = require('passport-auth0');
 // credentials for frontend server (for public requests to backend)
 var serverAuth = {
   client_id: process.env.AUTH0_SERVER_CLIENT_ID,
@@ -6,4 +7,32 @@ var serverAuth = {
   audience: process.env.AUTH0_SERVER_AUDIENCE
 }
 
-module.exports = {serverAuth};
+var userStrategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_USER_DOMAIN,
+    clientID: process.env.AUTH0_USER_CLIENT_ID,
+    clientSecret: process.env.AUTH0_USER_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_USER_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+
+    // add token to req.user for backend server requests
+    profile.accessToken = extraParams.access_token;
+    return done(null, profile);
+  }
+);
+
+var userAuthParams = {
+    clientID: process.env.AUTH0_USER_CLIENT_ID,
+    domain: process.env.AUTH0_USER_DOMAIN,
+    redirectUri: process.env.AUTH0_USER_CALLBACK_URL,
+    responseType: 'token id_token',
+    audience: process.env.AUTH0_USER_AUDIENCE,
+    scope: 'openid profile general'
+}
+
+module.exports = {serverAuth, userStrategy, userAuthParams};
--- hj-frontend-express-ejs/server.js
+++ hj-frontend-express-ejs/server.js
@@ -2,18 +2,42 @@
 const express = require('express');
 const request = require('superagent');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const ensureUserLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+
 dotenv.load();
 
-// server credentials and token caching
-const {serverAuth} = require('./auth/auth.js');
-let serverToken;
-let serverTokenTTL = 0;
+// load auth configs
+const {serverAuth,userStrategy,userAuthParams} = require('./auth/auth.js');
 
+let cachedServerAuthToken;
+let cachedServerAuthTokenTTL = 0;
+
+passport.use(userStrategy);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 // create express app
 const app = express();
 app.use(logger('dev'));
+app.use(cookieParser());
+// @todo conf session store
+app.use(
+  session({
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set the view engine to use EJS and the default views directory
 app.set('view engine', 'ejs');
@@ -23,9 +47,9 @@ app.set('views', __dirname + '/public/views/');
 app.use(express.static(__dirname + '/public'));
 
 // make a request to the oauth/token auth0 API for (general scope) access token for accessing backend
-function getAccessToken(req, res, next){
+function getServerAccessToken(req, res, next){
 
-  if(serverTokenTTL>new Date().getTime()) {
+  if(cachedServerAuthTokenTTL>new Date().getTime()) {
     req.access_token = serverToken;
     next();
   } else {
@@ -34,8 +58,8 @@ function getAccessToken(req, res, next){
       .send(serverAuth)
       .end(function(err, res) {
         if(res.body.access_token){
-          serverToken = res.body.access_token;
-          serverTokenTTL = new Date().getTime() + (1000 * 3500);
+          cachedServerAuthToken = res.body.access_token;
+          cachedServerAuthTokenTTL = new Date().getTime() + (1000 * 3500);
           req.access_token = res.body.access_token;
           next();
         } else {
@@ -48,16 +72,16 @@ function getAccessToken(req, res, next){
 // Public homepage without access control
 app.get('/', function(req, res){
   res.render('index');
-})
+});
 
 // static about page
 app.get('/about', function(req, res){
   res.render('about');
-})
+});
 
 // get token, add it to request header, get data and render it or deny
 // superagent does the handling of backend request
-app.get('/recipes', getAccessToken, function(req, res){
+app.get('/recipes', getServerAccessToken, function(req, res){
   request
     .get(process.env.BACKEND + '/recipes')
     .set('Authorization', 'Bearer ' + req.access_token)
@@ -69,10 +93,10 @@ app.get('/recipes', getAccessToken, function(req, res){
         res.render('recipes', { recipes: recipes} );
       }
     })
-})
+});
 
 // process is be the same for the remaining routes
-app.get('/chefs', getAccessToken, function(req, res){
+app.get('/chefs', getServerAccessToken, function(req, res){
   request
     .get(process.env.BACKEND + '/chefs')
     .set('Authorization', 'Bearer ' + req.access_token)
@@ -84,7 +108,41 @@ app.get('/chefs', getAccessToken, function(req, res){
         res.render('chefs', {chefs : chefs});
       }
     })
-})
+});
+
+// static about page
+app.get('/chefs/me', ensureUserLoggedIn, function(req, res){
+  res.render('me');
+});
+
+app.get('/login', passport.authenticate('auth0', userAuthParams),
+  function(req, res) {
+    res.redirect("/");
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get( '/callback',
+  passport.authenticate('auth0', {
+    failureRedirect: '/failure'
+  }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/chefs/me');
+  }
+);
+
+app.get('/failure', function(req, res) {
+  var error = req.flash("error");
+  var error_description = req.flash("error_description");
+  req.logout();
+  res.render('failure', {
+    error: error[0],
+    error_description: error_description[0],
+  });
+});
 
 // launch the frontend server
 app.listen(process.env.PORT, () => {
